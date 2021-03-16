5a4372c4431a31358ce90282d209a7ec3ca888f5
Heiko Mueller <heiko.muller@gmail.com>
2021-03-16 14:52:32 -0400

    Fix unit tests to not require Javascript build

--- tests/test_openclean_api.py
+++ tests/test_openclean_api.py
@@ -7,10 +7,42 @@
 
 """Unit tests for the openclean API extensions of the openclean engine."""
 
+import pkg_resources
+import pytest
+
+
 from openclean_notebook.engine import DB
 
 
-def test_edit_dataset(dataset, tmpdir):
+# -- Mock pkg_resources.resource_string ---------------------------------------
+
+@pytest.fixture
+def mock_pkg_resources(monkeypatch):
+    """Mock reading the build Javascript files."""
+    def mock_read_file(*args, **kwargs):
+        if args[1].endswith('.html'):
+            return """<html>
+            <head>
+            </head>
+            <body>
+                <script>
+                {bundle}
+                </script>
+                <div id="{id}">
+                </div>
+                <script>
+                    opencleanVis.renderOpencleanVisBundle("#{id}", {data});
+                </script>
+            </body>
+            </html>
+            """.encode('utf-8')
+        else:
+            return 'function(){};'.encode('utf-8')
+
+    monkeypatch.setattr(pkg_resources, "resource_string", mock_read_file)
+
+
+def test_edit_dataset(mock_pkg_resources, dataset, tmpdir):
     """Test to ensure that we can call the edit function without an error."""
     engine = DB(str(tmpdir))
     engine.create(source=dataset, name='DS', primary_key='A')
