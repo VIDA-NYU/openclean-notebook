77f1ff408ee602102f2cb2653c1760d6a22be3cf
Heiko Mueller <heiko.muller@gmail.com>
2021-03-19 06:30:35 -0400

    Include html templates in wheel

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,3 +4,8 @@
 ### 0.1.0 - 2021-15-18
 
 * Initial commit
+
+
+### 0.1.1 - 2021-15-19
+
+* Include html templates in wheel
--- openclean_notebook/version.py
+++ openclean_notebook/version.py
@@ -7,4 +7,4 @@
 
 """Version information for the openclean Jupyter package."""
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
